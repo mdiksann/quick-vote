7ab15acb3293d98cda9fbac39ae1cfc0520089f2
iksan <mdiksann@gmail.com>
2025-11-20 14:43:04 +0700

    feat: Add Option model with relationships and fillable attributes

---
+++ app/Models/Option.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Poll;
+use App\Models\Vote;
+
+class Option extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'poll_id',
+        'option_text',
+        'order',
+    ];
+
+    public function Poll(): BelongsTo
+    {
+        return $this->belongsTo(Poll::class);
+    }
+
+    public function Votes(): HasMany
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+    public function countVotes(): int
+    {
+        return $this->votes()->count();
+    }
+}
