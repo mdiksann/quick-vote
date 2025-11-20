d885e59b19c818131ac707c6ef622ea027997488
iksan <mdiksann@gmail.com>
2025-11-20 14:43:32 +0700

    feat: Add Vote model with relationships and fillable attributes

---
+++ app/Models/Vote.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
+use App\Models\Poll;
+use App\Models\Option;
+
+class Vote extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'poll_id',
+        'option_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function poll(): BelongsTo
+    {
+        return $this->belongsTo(Poll::class);
+    }
+
+    public function option(): BelongsTo
+    {
+        return $this->belongsTo(Option::class);
+    }
+}
