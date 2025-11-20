49c15a01dadaa28edaba08dea218e9f71a2fea59
iksan <mdiksann@gmail.com>
2025-11-20 14:43:48 +0700

    feat: Add Subscription model with relationships and fillable attributes

---
+++ app/Models/Subscription.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
+use App\Models\Poll;
+
+class Subscription extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'poll_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function poll(): BelongsTo
+    {
+        return $this->belongsTo(Poll::class);
+    }
+}
