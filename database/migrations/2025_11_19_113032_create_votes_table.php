dbec65b47e491711021219619e3be97f6b9a692a
iksan <mdiksann@gmail.com>
2025-11-20 14:43:41 +0700

    feat: Create migration for votes table with relationships and attributes

---
+++ database/migrations/2025_11_19_113032_create_votes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('poll_id')->constrained('polls')->onDelete('cascade');
+            $table->foreignId('option_id')->constrained('options')->onDelete('cascade');
+            $table->timestamps();
+            $table->unique(['user_id', 'poll_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('votes');
+    }
+};
